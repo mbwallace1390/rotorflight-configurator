d8a31d924f9edd418d24acde8f71767155beab23
Michael Wallace <mbwallace1390@gmail.com>
2026-07-06 23:03:50 -0500

    Add USB mass-storage log picker bridge

--- cordova/plugins/cordova-plugin-rotorflight-blackbox-intent/www/blackboxIntent.js
+++ cordova/plugins/cordova-plugin-rotorflight-blackbox-intent/www/blackboxIntent.js
@@ -3,3 +3,7 @@ const cordovaExec = require("cordova/exec");
 exports.open = function (fileUrl, onSuccess, onError) {
   cordovaExec(onSuccess, onError, "BlackboxIntent", "open", [fileUrl]);
 };
+
+exports.pickAndOpen = function (onSuccess, onError) {
+  cordovaExec(onSuccess, onError, "BlackboxIntent", "pickAndOpen", []);
+};
